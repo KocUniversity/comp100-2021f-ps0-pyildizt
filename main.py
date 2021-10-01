60fb9cc45b7e2e33d2ef1d33b0701e2049d95e19
PERIHAN YILDIZ TOPAL <91727590+pyildizt@users.noreply.github.com>
2021-10-01 09:49:47 +0000

    I finished the first hw

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy
+
+x=int(input("Enter number x:"))
+y=int(input("Enter number y:"))
+
+print(x , "**", y , " = ", x**y)
+
+answer= numpy.log2(x)
+print("log(",x,") = ", answer)
+
+print("79920")
